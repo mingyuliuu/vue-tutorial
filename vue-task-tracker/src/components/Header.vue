9c814ef6aaf3204fb3c0eb7667c4a5c571d52ee7
Mingyu Liu <mingyuliu88@gmail.com>
2022-12-17 23:16:18 -0500

    Set up basic task tracker; Using Vue Cli

---
+++ vue-task-tracker/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <ButtonComponent text="Add Task" color="green" />
+    <ButtonComponent text="Update Task" color="blue" />
+    <ButtonComponent text="Delete Task" color="red" />
+  </header>
+</template>
+
+<script>
+import ButtonComponent from "./Button.vue";
+
+export default {
+  name: "HeaderItem",
+  props: {
+    title: {
+      type: String,
+      default: "Hello World",
+    },
+  },
+  components: {
+    ButtonComponent,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
